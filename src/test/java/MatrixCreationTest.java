45d580420e746f710a765e5d45d203c5e6dd14ae
Dmytry Dyachenko <dmytry.dyachenko@teamdev.local>
2017-12-08 15:01:24 +0200

    Added some Test.

--- src/test/java/MatrixCreationTest.java
+++ src/test/java/MatrixCreationTest.java
@@ -1,4 +1,3 @@
-import javaclasses.encryptor.Encryptor;
 import javaclasses.encryptor.impl.EncryptorImpl;
 import org.junit.Test;
 
@@ -8,11 +7,17 @@ import static org.junit.Assert.assertEquals;
 
 public class MatrixCreationTest {
     EncryptorImpl encryptor = new EncryptorImpl();
+
     @Test
     public void testColumnAndRowSize() {
-       // List<Integer> sizes = encryptor.getColumnAndRowLength(10);
-        encryptor.encrypt("if man was meant to stay on the ground god would have given us roots");
-        /*assertEquals(3, sizes.get(0),0.001);
-        assertEquals(4, sizes.get(1),0.001);*/
+        List<Integer> sizes = encryptor.getColumnAndRowLength(10);
+        assertEquals(3, sizes.get(0), 0.001);
+        assertEquals(4, sizes.get(1), 0.001);
+    }
+
+    @Test
+    public void testTextForEncryption() {
+        final String result = encryptor.encrypt("have a nice day");
+        assertEquals(result, "hae and via ecy ");
     }
 }
